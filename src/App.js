f991cec9cea0fc80552e13964e10cbdcc9db978f
arinkhan25 <akhan0119@richmondhillhs.org>
2020-12-11 18:56:44 +0000

    arin furbook

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
 import PetNav from './components/pet-nav'
 import PictureBox from './components/picture-box'
+import AboutBox from './components/about-box';
+import Posts from './components/posts';
 import './App.css';
-
 const App = () => {
   const component = new React.Component()
-
   component.render = () => {
     return(
       <div className="App">
         <PetNav/>
         <div class="container">
           <div class="row">
-            //Add profile picture and about section here
+            <PictureBox/>
+            <AboutBox/>
           </div>
           <div class="row">
-            //Add posts here
+            <Posts/>
           </div>
         </div>
       </div>
     )
   }
-
   return component
 }
-
 export default App;
